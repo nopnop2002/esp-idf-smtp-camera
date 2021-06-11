2bd28c7a2c801535dd301cd4dda33bce56a07a8d
nopnop2002 <hau26280@tree.odn.ne.jp>
2021-06-11 11:33:30 +0900

    Changed tab stop

--- main/cmd.h
+++ main/cmd.h
@@ -3,13 +3,13 @@
 #define CMD_HALT 400
 
 typedef struct {
-    uint16_t command;
-    TaskHandle_t taskHandle;
+	uint16_t command;
+	TaskHandle_t taskHandle;
 } CMD_t;
 
 typedef struct {
-    uint16_t command;
-    TaskHandle_t taskHandle;
+	uint16_t command;
+	TaskHandle_t taskHandle;
 	char localFileName[64];
 	size_t localFileSize;
 	char remoteFileName[64];
